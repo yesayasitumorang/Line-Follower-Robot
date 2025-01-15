984e9b154387d30ee69e83c2654eea15d60a2717
yesayastm <73650854+yesayasitumorang@users.noreply.github.com>
2025-01-15 21:47:53 +0700

    Update LineFollowerTransporter1.ino

--- LineFollowerTransporter1.ino
+++ LineFollowerTransporter1.ino
@@ -22,7 +22,6 @@ int rintangan = 0;  // Variabel untuk menghitung jumlah simpang yang terdeteksi
 bool sensorAktifSebelumnya = false;  // Menyimpan status kelima sensor aktif sebelumnya
 bool flag = false;  // Flag untuk menandakan robot berhenti sejenak
 int majuCepat = 250;
-int maju = 200;
 int belokCepat = 200;
 int belok = 180;
 
@@ -73,220 +72,96 @@ void loop() {
     // Menambahkan flag untuk berhenti sejenak sebelum menjalankan case
     if (!flag) {
       stop();  // Berhenti dulu
-      delay(500);  // Tunggu sebentar sebelum melanjutkan
+      delay(300);  // Tunggu sebentar sebelum melanjutkan
       flag = true;  // Set flag agar perintah case dijalankan
     }
 
     // Menangani simpang sesuai dengan nilai simpang
     switch(rintangan) {
       case 1://start(maju)
+        stop();
+        delay(1000);
+        smoothMove(0, 180, 5); // Dari 0° ke 180° dengan langkah 2°
+        delay(1500);
         digitalWrite(MOTOR_A_DIR, HIGH);
         digitalWrite(MOTOR_B_DIR, HIGH);
         analogWrite(MOTOR_A_SPEED, majuCepat);  // Kecepatan belok maju
         analogWrite(MOTOR_B_SPEED, majuCepat);  // Kecepatan belok maju
-        delay(100);
+        delay(500);
         break;
       case 2://simpang 1 (belok kiri)
         digitalWrite(MOTOR_A_DIR, HIGH);
         digitalWrite(MOTOR_B_DIR, HIGH);
         analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, maju);  // Kecepatan belok maju
-        delay(1500);
+        analogWrite(MOTOR_B_SPEED, 250);  // Kecepatan belok maju
+        delay(500);
         break;
-      case 4://simpang 2 (ambil pohon dan belok kiri)
+      case 3://simpang 2 (ambil pohon dan belok kiri)
         digitalWrite(MOTOR_A_DIR, LOW);
         digitalWrite(MOTOR_B_DIR, LOW);
         analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok maju
         analogWrite(MOTOR_B_SPEED, 0);  // Kecepatan belok maju
-        delay(300);
+        delay(1000);
         digitalWrite(MOTOR_A_DIR, HIGH);
         digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, majuCepat);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, majuCepat);
-        delay(500);
-      case 5: //simpang setelah simpang 2
-        digitalWrite(MOTOR_A_SPEED, LOW);
-        digitalWrite(MOTOR_B_SPEED, LOW);
+        analogWrite(MOTOR_A_SPEED, 250);  
+        analogWrite(MOTOR_B_SPEED, 250);
+        delay(100);
+        majuLambat();
+        delay(2000);
         analogWrite(MOTOR_A_SPEED, 0);
         analogWrite(MOTOR_B_SPEED, 0);
-        delay(400);
+        delay(700);
         digitalWrite(MOTOR_A_DIR, LOW);
         digitalWrite(MOTOR_B_DIR, LOW);
         analogWrite(MOTOR_A_SPEED, 200);
         analogWrite(MOTOR_B_SPEED, 200);
-        delay(300);             
+        delay(100);             
           // Gerakkan servo dari 0° ke 180° dengan langkah kecil
-        Serial.println("Servo bergerak dari 0 ke 50 derajat");
-        smoothMove(180, 90, 2); // Dari 0° ke 180° dengan langkah 2°
+        Serial.println("Servo bergerak dari 0 ke 180 derajat");
+        smoothMove(0, 180, 2); // Dari 0° ke 180° dengan langkah 2°
         delay(1000);
 
         digitalWrite(MOTOR_A_DIR, LOW);
         digitalWrite(MOTOR_B_DIR, LOW);
         analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok maju
         analogWrite(MOTOR_B_SPEED, 0);  // Kecepatan belok maju
-
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, 180);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, 180);  // Kecepatan belok maju
-        delay(100);
-
-        digitalWrite(MOTOR_A_DIR, LOW);
-        digitalWrite(MOTOR_B_DIR, LOW);
-        analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, 0);  // Kecepatan belok maju
+        delay(1000);
 
           // Gerakkan servo dari 180° ke 0° dengan langkah kecil
         Serial.println("Servo kembali dari 180 ke 0 derajat");
-        smoothMove(90, 180, 2); // Dari 180° ke 0° dengan langkah 2°
+        smoothMove(180, 0, 2); // Dari 180° ke 0° dengan langkah 2°
         delay(1000);
-
         analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok maju
         analogWrite(MOTOR_B_SPEED, 0);  // Kecepatan belok maju
         delay(1000);
-
         digitalWrite(MOTOR_A_DIR, HIGH);
         digitalWrite(MOTOR_B_DIR, HIGH);
         analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok maju
         analogWrite(MOTOR_B_SPEED, 255);  // Kecepatan belok maju
         delay(700);
         break;
-      case 7:
+      case 4:
         stop();
-        delay(100);
+        delay(600);
         mundur();
-        delay(400);
+        delay(200);
         Serial.println("Servo kembali dari 180 ke 0 derajat");
-        smoothMove(90, 180, 2); // Dari 180° ke 0° dengan langkah 2°
-        delay(700);
+        smoothMove(0, 180, 2); // Dari 180° ke 0° dengan langkah 2°
+        delay(1000);
         mundur();
-        delay(900);
+        delay(1000);
         belokKanan();
         delay(300);
-
-      /* case ://simpang 3 (belok kanan & taruh object)
-        digitalWrite(MOTOR_A_DIR, HIGH);   
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, 0);  // Kecepatan belok maju
-        delay(1500);
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, 255);  // Kecepatan belok maju
-        delay(500);
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, 255);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, 255);  // Kecepatan belok maju
-        delay(10);
-        break; */
-      case 9:// simpang 4 (belok kanan)
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok kanan
-        analogWrite(MOTOR_B_SPEED, 255);  // Kecepatan belok kanan
-        delay(1000);
-        break;
-      case 13:
+      case 5:
         digitalWrite(MOTOR_A_DIR, LOW);
         digitalWrite(MOTOR_B_DIR, LOW);
         analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok kanan
         analogWrite(MOTOR_B_SPEED, 0);  // Kecepatan belok kanan
-        delay(10000000000);
       default:
         Serial.println("Kasus tidak ditangani!");
         break;
     }
-  }
-   else if (s2 == HIGH && s3 == HIGH && s4 == HIGH) {  // case check point
-    if (!sensorAktifSebelumnya) {  // Jika sensor sebelumnya tidak aktif
-      rintangan++;  // Tambah simpang
-      sensorAktifSebelumnya = true;  // Tandai bahwa sensor sudah aktif
-      Serial.print("rintangan: "); Serial.println(rintangan);  // Debugging nilai simpang
-    }
-    if (!flag) {
-      stop();  // Berhenti dulu
-      delay(1000);  // Tunggu sebentar sebelum melanjutkan
-      flag = true;  // Set flag agar perintah case dijalankan
-    }
-     switch(rintangan) {
-      case 3://checkpoint 1
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, majuCepat);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, majuCepat);  // Kecepatan belok maju
-        delay(100);
-        break;
-      case 6://checkpoint 2
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, belokCepat);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, 0);  // Kecepatan belok maju
-        delay(300);
-        break;
-      case 8://checkpoint 3
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, majuCepat);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, majuCepat);  // Kecepatan belok maju
-        delay(50);
-        break;
-  } 
-   }
-  else if (s3 == HIGH && s4 == HIGH && s5 == HIGH) {  //case belok kanan
-    if (!sensorAktifSebelumnya) {  // Jika sensor sebelumnya tidak aktif
-      rintangan++;  // Tambah simpang
-      sensorAktifSebelumnya = true;  // Tandai bahwa sensor sudah aktif
-      Serial.print("rintangan: "); Serial.println(rintangan);  // Debugging nilai simpang
-    }
-    if (!flag) {
-      stop();  // Berhenti dulu
-      delay(1000);  // Tunggu sebentar sebelum melanjutkan
-      flag = true;  // Set flag agar perintah case dijalankan
-    }
-     switch(rintangan) {
-      case 11://belok kanan 1
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, majuCepat);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, majuCepat);  // Kecepatan belok maju
-        delay(100);
-        break;
-     }
-  } else if (s1 == HIGH && s2 == HIGH && s3 == HIGH) {  //case belok kiri
-    if (!sensorAktifSebelumnya) {  // Jika sensor sebelumnya tidak aktif
-      rintangan++;  // Tambah simpang
-      sensorAktifSebelumnya = true;  // Tandai bahwa sensor sudah aktif
-      Serial.print("rintangan: "); Serial.println(rintangan);  // Debugging nilai simpang
-    }
-    if (!flag) {
-      stop();  // Berhenti dulu
-      delay(300);  // Tunggu sebentar sebelum melanjutkan
-      flag = true;  // Set flag agar perintah case dijalankan
-    }
-     switch(rintangan) {
-      case 9://kiri 1
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, 0);
-        analogWrite(MOTOR_B_SPEED, 255);
-        delay(300);
-        break;
-      case 10://kiri 2
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, 255);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, 255);  // Kecepatan belok maju
-        delay(100);
-        break;
-      case 12:
-        digitalWrite(MOTOR_A_DIR, HIGH);
-        digitalWrite(MOTOR_B_DIR, HIGH);
-        analogWrite(MOTOR_A_SPEED, 255);  // Kecepatan belok maju
-        analogWrite(MOTOR_B_SPEED, 255);
-        delay(100);
-     }
   } else {  // Jika sensor tidak aktif
     sensorAktifSebelumnya = false;  // Reset status sensor
     flag = false;  // Reset flag agar perintah case bisa dijalankan kembali ketika ada simpang baru
@@ -294,33 +169,42 @@ void loop() {
 
 
   // Logika untuk gerakan robot lainnya (sama seperti sebelumnya)
- if (s1 == LOW && s2 == LOW && s3 == LOW && s4 == LOW && s5 == LOW) {  // Maju jika sensor tengah aktif
-    mundur();
-  } else if (s3 == HIGH) {  // maju
-  digitalWrite(MOTOR_A_DIR, HIGH);
-  digitalWrite(MOTOR_B_DIR, HIGH);
-  analogWrite(MOTOR_A_SPEED, 255);  // Kecepatan maksimal
-  analogWrite(MOTOR_B_SPEED, 255);  
+ if (s3 == HIGH) {  // maju
+    digitalWrite(MOTOR_A_DIR, HIGH);
+    digitalWrite(MOTOR_B_DIR, HIGH);
+    analogWrite(MOTOR_A_SPEED, 255);  // Kecepatan maksimal
+    analogWrite(MOTOR_B_SPEED, 255);  
   } else if (s1 == HIGH && s5 == HIGH) {  // maju
-  digitalWrite(MOTOR_A_DIR, HIGH);
-  digitalWrite(MOTOR_B_DIR, HIGH);
-  analogWrite(MOTOR_A_SPEED, 255);  // Kecepatan maksimal
-  analogWrite(MOTOR_B_SPEED, 255);
+    digitalWrite(MOTOR_A_DIR, HIGH);
+    digitalWrite(MOTOR_B_DIR, HIGH);
+    analogWrite(MOTOR_A_SPEED, 255);  // Kecepatan maksimal
+    analogWrite(MOTOR_B_SPEED, 255);
   } else if (s1 == HIGH || s2 == HIGH) {  // Belok kiri jika sensor kiri aktif
     digitalWrite(MOTOR_A_DIR, HIGH);
     digitalWrite(MOTOR_B_DIR, HIGH);
     analogWrite(MOTOR_A_SPEED, 0);  // Kecepatan belok kiri
-    analogWrite(MOTOR_B_SPEED, 255);
+    analogWrite(MOTOR_B_SPEED, 240);
   } else if (s4 == HIGH || s5 == HIGH) {  // Belok kanan jika sensor kanan aktif
     digitalWrite(MOTOR_A_DIR, HIGH);
     digitalWrite(MOTOR_B_DIR, HIGH);
-    analogWrite(MOTOR_A_SPEED, 255);  // Kecepatan belok kanan
+    analogWrite(MOTOR_A_SPEED, 240);
     analogWrite(MOTOR_B_SPEED, 0);
+  } else if (s3 == HIGH && s4 == HIGH && s5 == HIGH) {
+    if (rintangan == 3 || rintangan == 4) {
+      belokKanan();
+      delay(400);
+    } else {
+      digitalWrite(MOTOR_A_DIR, HIGH);
+      digitalWrite(MOTOR_B_DIR, HIGH);
+      analogWrite(MOTOR_A_SPEED, majuCepat);  // Kecepatan belok kanan
+      analogWrite(MOTOR_B_SPEED, majuCepat);
+    }
+  } else if (s1 == HIGH && s2 == HIGH && s3 == HIGH) {
+      maju();
+      delay(100);
   } else {  // Berhenti jika tidak ada garis
-    digitalWrite(MOTOR_A_DIR, LOW);
-    digitalWrite(MOTOR_B_DIR, LOW);
-    analogWrite(MOTOR_A_SPEED, 200);
-    analogWrite(MOTOR_B_SPEED, 200);
+    mundur();
+    delay(1000);
   }
 }
 
@@ -331,10 +215,6 @@ void maju() {
   analogWrite(MOTOR_A_SPEED, 255);  // Kecepatan maksimal
   analogWrite(MOTOR_B_SPEED, 255);  // Kecepatan maksimal
 }
-
-
-
-
 
  */
  void belokKiri() {
@@ -363,6 +243,20 @@ void stop() {
   digitalWrite(MOTOR_B_DIR, LOW);
   analogWrite(MOTOR_A_SPEED, 0);
   analogWrite(MOTOR_B_SPEED, 0); // Berhenti dengan mematikan motor
+}
+
+void maju() {
+  digitalWrite(MOTOR_A_DIR, HIGH);
+  digitalWrite(MOTOR_B_DIR, HIGH);
+  analogWrite(MOTOR_A_SPEED, majuCepat);
+  analogWrite(MOTOR_B_SPEED, majuCepat);
+}
+
+void majuLambat() {
+  digitalWrite(MOTOR_A_DIR, HIGH);
+  digitalWrite(MOTOR_B_DIR, HIGH);
+  analogWrite(MOTOR_A_SPEED, 180);
+  analogWrite(MOTOR_B_SPEED, 180);
 }
 
 void smoothMove(int startAngle, int endAngle, int step) {
